e2cd704b2e881bd397a7ede1c8d64b44c37dc9af
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-16 12:21:32 +0100

    Update um.php

---
+++ um.php
@@ -0,0 +1,14 @@
+<?php
+$temperatura  = 0;
+echo '<h1> A temperatura é de'. $temperatura. 'º<h1>';
+if ($temperatura <=3) {
+	echo "Dia Gelado";
+}
+ 
+ elseif ($temperatura > 3 && $temperatura <=15 ) {
+ 	echo "Dia Frio";
+ }
+
+ elseif () {
+ 	# code...
+ }
